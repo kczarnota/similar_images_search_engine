43bc6a129883aa01490aa347c358a872d6878ba3
Konrad <k.czarnota27@gmail.com>
2016-05-21 18:25:58 +0200

    Delete all those duplicate files

--- BazaDanych/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/konrad/Dokumenty/CLionProjects/BagOfWords
-# Build directory: /home/konrad/Dokumenty/CLionProjects/BagOfWords/BazaDanych
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestProject "Test")
