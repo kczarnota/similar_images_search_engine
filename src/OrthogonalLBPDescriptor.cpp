2cafea1b68bc249b6fab1c3ba6b86db590157430
Konrad <k.czarnota27@gmail.com>
2016-11-04 22:30:47 +0100

    Implement OrthogonalLBP

--- src/OrthogonalLBPDescriptor.cpp
+++ src/OrthogonalLBPDescriptor.cpp
@@ -2,13 +2,13 @@
 
 void OrthogonalLBPDescriptor::computeOrthogonalLBPfeatures(Mat image, Mat lbpFeatures, vector<KeyPoint> keyPoints)
 {
-    int counter = 0;
     for(KeyPoint p : keyPoints)
     {
         Mat dest;
         getRectSubPix(image, Size(10, 10), Point2f(p.pt.x, p.pt.y), dest);
 
         int flatNumber = 0;
+        Mat currentFeatures = Mat(1, 128, CV_32FC1, Scalar(0));
         for(int r = 1; r < 9; ++r)
         {
             for (int c = 1; c < 9; ++c)
@@ -16,55 +16,58 @@ void OrthogonalLBPDescriptor::computeOrthogonalLBPfeatures(Mat image, Mat lbpFea
                 Scalar intensity = dest.at<uchar>(r, c);
                 uchar mid = intensity.val[0];
 
-                unsigned char number = 0;
+                unsigned char numberOne = 0;
+                unsigned char numberTwo = 0;
 
                 //right middle
                 intensity = dest.at<uchar>(r, c + 1);
                 if(intensity.val[0] > mid)
                 {
-                    number |= 1;
-                }
-
-                //rigth bottom
-                intensity = dest.at<uchar>(r + 1, c + 1);
-                if(intensity.val[0] > mid)
-                {
-                    number |= 1 << 1;
+                    numberOne |= 1;
                 }
 
                 //bottom middle
                 intensity = dest.at<uchar>(r + 1, c);
                 if(intensity.val[0] > mid)
                 {
-                    number |= 1 << 2;
-                }
-
-                //left bottom
-                intensity = dest.at<uchar>(r + 1, c - 1);
-                if(intensity.val[0] > mid)
-                {
-                    number |= 1 << 3;
+                    numberOne |= 1 << 1;
                 }
 
                 //left middle
                 intensity = dest.at<uchar>(r, c - 1);
                 if(intensity.val[0] > mid)
                 {
-                    number |= 1 << 4;
-                }
-
-                //left top
-                intensity = dest.at<uchar>(r - 1, c - 1);
-                if(intensity.val[0] > mid)
-                {
-                    number |= 1 << 5;
+                    numberOne |= 1 << 2;
                 }
 
                 //right middle
                 intensity = dest.at<uchar>(r - 1, c);
                 if(intensity.val[0] > mid)
                 {
-                    number |= 1 << 6;
+                    numberOne |= 1 << 3;
+                }
+
+                //rigth bottom
+                intensity = dest.at<uchar>(r + 1, c + 1);
+                if(intensity.val[0] > mid)
+                {
+                    numberTwo |= 1;
+                }
+
+
+                //left bottom
+                intensity = dest.at<uchar>(r + 1, c - 1);
+                if(intensity.val[0] > mid)
+                {
+                    numberTwo |= 1 << 1;
+                }
+
+
+                //left top
+                intensity = dest.at<uchar>(r - 1, c - 1);
+                if(intensity.val[0] > mid)
+                {
+                    numberTwo |= 1 << 2;
                 }
 
 
@@ -72,14 +75,16 @@ void OrthogonalLBPDescriptor::computeOrthogonalLBPfeatures(Mat image, Mat lbpFea
                 intensity = dest.at<uchar>(r - 1, c + 1);
                 if(intensity.val[0] > mid)
                 {
-                    number |= 1 << 7;
+                    numberTwo |= 1 << 3;
                 }
 
 
-                lbpFeatures.at<float>(counter, flatNumber) = number;
+                currentFeatures.at<float>(0, flatNumber) = numberOne;
+                flatNumber++;
+                currentFeatures.at<float>(0, flatNumber) = numberTwo;
                 flatNumber++;
             }
         }
-        counter++;
+        vconcat(currentFeatures, lbpFeatures, lbpFeatures);
     }
 }
